076480d4a0eff9aa298c5640a4e914bbaad81986
Carolina Gonçalves <contato.devcarolina@gmail.com>
2023-11-29 17:58:18 -0300

    feat: Add slideshow effect to photo list component

--- src/app/photo-list/photo-list.component.ts
+++ src/app/photo-list/photo-list.component.ts
@@ -12,10 +12,31 @@ import { IPhoto, getPhotos } from '../services/photos-services';
   styleUrl: './photo-list.component.css',
 })
 export class PhotoListComponent implements OnInit {
-
   photos: IPhoto[] = [];
+  currentPhotoIndex: number = 0;
 
+  /**
+   * Inicializa o componente e carrega as fotos.
+   */
   async ngOnInit() {
     this.photos = await getPhotos();
+  }
+
+  /**
+   * Navega para a próxima foto.
+   */
+  navigateForward() {
+    if (this.currentPhotoIndex < this.photos.length - 1) {
+      this.currentPhotoIndex++;
+    }
+  }
+
+  /**
+   * Navega para a foto anterior.
+   */
+  navigateBackward() {
+    if (this.currentPhotoIndex > 0) {
+      this.currentPhotoIndex--;
+    }
   }
 }
